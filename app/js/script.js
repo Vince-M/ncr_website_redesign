19d2fa1bfdaf7d790ee58f471f9acd3fabbeb6bf
Vince Mendella <vmendella@gmail.com>
2024-10-23 17:59:33 -0500

    style tonNav menu. JS for open and close mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
 console.log('HELLO');
+
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+function openMobileMenu() {
+  console.log('run open mobile menu');
+  btnOpen.setAttribute('aria-expanded', 'true');
+}
+
+function closeMobileMenu() {
+  console.log('run close mobile menu');
+  btnOpen.setAttribute('aria-expanded', 'false');
+}
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("HELLO");
+"use strict";console.log("HELLO");var btnOpen=document.querySelector("#btnOpen"),btnClose=document.querySelector("#btnClose");function openMobileMenu(){console.log("run open mobile menu"),btnOpen.setAttribute("aria-expanded","true")}function closeMobileMenu(){console.log("run close mobile menu"),btnOpen.setAttribute("aria-expanded","false")}btnOpen.addEventListener("click",openMobileMenu),btnClose.addEventListener("click",closeMobileMenu);
 //# sourceMappingURL=script.js.map
