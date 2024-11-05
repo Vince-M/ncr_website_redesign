53e3f12cd51636213be8637ca2efd826b4b9f522
Vince Mendella <vmendella@gmail.com>
2024-11-04 18:14:43 -0600

    apply revisions to mobile menu so opens and closes for accessiblity, also make so you can click links in mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,58 @@
-console.log('HELLO');
+// console.log('HELLO');
 
+const main = document.querySelector('#main');
+const footer = document.querySelector('#footer');
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
+const menuTopNav = document.querySelector('#menuTopNav');
+const overlay = document.querySelector('#overlay');
+const breakpoint = window.matchMedia('(width < 30em)');
+
+const setupTopNav = () => {
+  if (breakpoint.matches) {
+    // console.log('is mobile');
+    menuTopNav.setAttribute('inert', '');
+  }
+  else {
+    // console.log('is tablet/desktop');
+    menuTopNav.removeAttribute('inert');
+  }
+}
+
+setupTopNav();
 
 btnOpen.addEventListener('click', openMobileMenu);
 btnClose.addEventListener('click', closeMobileMenu);
 
+breakpoint.addEventListener('change', () => {
+  // console.log('breakpoint crossed');
+  setupTopNav();
+});
+
 function openMobileMenu() {
-  console.log('run open mobile menu');
+  // console.log('run open mobile menu');
   btnOpen.setAttribute('aria-expanded', 'true');
+  main.setAttribute('inert', '');
+  footer.setAttribute('inert', '');
+  menuTopNav.removeAttribute('inert');
+  menuTopNav.style.transitionDuration = '400ms';
+  overlay.style.transitionDuration = '400ms';
+  bodyScrollLock.disableBodyScroll(menuTopNav);
+  btnClose.focus();
 }
 
 function closeMobileMenu() {
-  console.log('run close mobile menu');
+  // console.log('run close mobile menu');
   btnOpen.setAttribute('aria-expanded', 'false');
+  main.removeAttribute('inert');
+  footer.removeAttribute('inert');
+  menuTopNav.setAttribute('inert', '');
+  bodyScrollLock.enableBodyScroll(menuTopNav);
+  btnOpen.focus();
+  
+  setTimeout(() => {
+    menuTopNav.removeAttribute('style');
+    overlay.removeAttribute('style');
+  }, 500);
 }
+
